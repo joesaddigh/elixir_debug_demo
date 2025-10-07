34530f005d2ed4b15c9fd1ab5b9232bc4130d758
Joe Saddigh <39386448+joesaddigh@users.noreply.github.com>
2025-10-07 16:28:07 +0100

    Remove comment from logger configuration
    Removed comment about console format in logger config

--- config/dev.exs
+++ config/dev.exs
@@ -3,7 +3,6 @@ import Config
 
 config :logger, level: :info
 
-# Simple, readable console format. Add metadata if you like.
 config :logger, :console,
   format: "$time [$level] $message\n",
   color: [enabled: true]
